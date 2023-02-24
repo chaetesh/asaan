bd582a423d5f27e8113d500692c6aa66666987d3
chaetesh <chaetesh@gmail.com>
2023-02-25 02:07:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Faq from "./components/Faq";
-import Functionalities from "./components/Functionalities";
 import Navbar from "./components/Navbar";
-import { Slide } from "./components/Slider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Health from "./components/Health";
